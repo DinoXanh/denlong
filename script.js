ecb85474e2ae30f5196aae2349f0e2fa35b2947e
DinoXanh <mumcip99@gmail.com>
2026-06-23 18:26:38 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const den = document.getElementById("den");
+
+den.addEventListener("mouseover", function () {
+    den.style.animationPlayState = "paused";
+});
+
+den.addEventListener("mouseout", function () {
+    den.style.animationPlayState = "running";
+});
